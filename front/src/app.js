78d77c2b0b4f5b66259590395329dd7c0b3d07cd
preigile <diva.artim@gmail.com>
2018-05-12 22:34:08 +0300

    base structure

---
+++ front/src/app.js
@@ -0,0 +1,4 @@
+import Header from './components/Header'
+import './styles/main.less'
+
+console.log(Header());
---
+++ front/src/components/Header.js
@@ -0,0 +1,4 @@
+export default () => {
+    constructor();
+    console.log('header');
+}
