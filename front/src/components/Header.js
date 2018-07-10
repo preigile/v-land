25d2426e3968df917f79cf24c9401fc6680e4e37
preigile <diva.artim@gmail.com>
2018-07-10 21:52:40 +0300

    change logo in header

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,4 +1,9 @@
 export default () => {
-    constructor();
     console.log('header');
+    return `<div class="header">
+                <h3 class="logo">Body Time</h3>
+                <div class="phone-container">
+                    <a class="phone" href="tel:+7999999999">+7 (999) 999-99-99</a>
+                </div>
+            </div>`
 }
